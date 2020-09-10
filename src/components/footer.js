b88eed8b2b5f70f56cd7a9e8351ca2df70e207b8
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-09-09 22:42:44 -0700

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const SOURCE_URL = 'https://github.com/Liversticks/meal-client'
+
+
+function Footer(props) {
+  return (
+    <div className="mt-3 mb-3 text-center">
+      Made by Liversticks. Check out the source code <a href={SOURCE_URL}>here.</a>
+    </div>
+  )
+}
+
+export default Footer
