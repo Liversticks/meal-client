c50e01b8d02eba8673b6a189525fa4118a9f1b5d
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-09-22 21:12:10 -0700

    add users api route

--- src/config/local-dev.js
+++ src/config/local-dev.js
@@ -2,6 +2,7 @@ let localConfig = {
   meal_url: 'http://localhost:5000/api/meals',
   login_url: 'http://localhost:5000/api/login',
   signup_url: 'http://localhost:5000/api/signup',
+  users_url: 'http://localhost:5000/api/users'
 }
 
 export default localConfig
--- src/config/prod.js
+++ src/config/prod.js
@@ -2,6 +2,7 @@ let prodConfig = {
   meal_url: 'https://ls-meal-app.herokuapp.com/api/meals',
   login_url: 'https://ls-meal-app.herokuapp.com/api/login',
   signup_url: 'https://ls-meal-app.herokuapp.com/api/signup',
+  users_url: 'https://ls-meal-app.herokuapp.com/api/users'
 }
 
 export default prodConfig
