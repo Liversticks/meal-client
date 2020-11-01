e58d0d21fb850e53ed0c7d1a1b4733b22e09a60b
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-10-31 21:52:02 -0700

    config changes

--- src/config/local-dev.js
+++ src/config/local-dev.js
@@ -1,8 +1,9 @@
 let localConfig = {
   meal_url: 'http://localhost:5000/api/meals',
-  login_url: 'http://localhost:5000/api/login',
-  signup_url: 'http://localhost:5000/api/signup',
-  users_url: 'http://localhost:5000/api/users'
+  login_url: 'http://localhost:5000/api/auth/login',
+  signup_url: 'http://localhost:5000/api/auth/signup',
+  users_url: 'http://localhost:5000/api/users',
+  static_dir: 'http://localhost:5000/static/',
 }
 
 export default localConfig
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,8 +1,9 @@
 let prodConfig = {
   meal_url: 'https://ls-meal-app.herokuapp.com/api/meals',
-  login_url: 'https://ls-meal-app.herokuapp.com/api/login',
-  signup_url: 'https://ls-meal-app.herokuapp.com/api/signup',
-  users_url: 'https://ls-meal-app.herokuapp.com/api/users'
+  login_url: 'https://ls-meal-app.herokuapp.com/api/auth/login',
+  signup_url: 'https://ls-meal-app.herokuapp.com/api/auth/signup',
+  users_url: 'https://ls-meal-app.herokuapp.com/api/users',
+  static_dir: 'https://ls-meal-app.herokuapp.com/static/',
 }
 
 export default prodConfig
